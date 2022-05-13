7a3c3c852f717d9c269a355a84243dd7bbbb73fa
Colton <coltonsmith28@gmail.com>
2022-05-13 06:51:30 -0500

    Added boardimage comp

--- src/components/Board/BoardImage/BoardImage.js
+++ src/components/Board/BoardImage/BoardImage.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import placeholder from '../../../assets/images/placeholder.jpg'
+import './BoardImage.scss';
 
 const BoardImage = () => {
   return (
-    <div>
-      
+    <div className='bi'>
+      <img className='bi__image' src={placeholder} alt="image" />
+      <img className='bi__image' src={placeholder} alt="image" />
+      <img className='bi__image' src={placeholder} alt="image" />
+      <img className='bi__image' src={placeholder} alt="image" />
     </div>
   );
 };
