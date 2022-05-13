ae758ec812fa5d6907e021dbc56a1a27a3780184
Colton <coltonsmith28@gmail.com>
2022-05-13 08:47:50 -0500

    Added collaborators page

---
+++ src/pages/Spotlight/Collaborators/Collaborators.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Collaborators extends Component {
+  render() {
+    return (
+      <div className='collab'>
+        
+      </div> 
+    );
+  }
+}
+
+export default Collaborators;
