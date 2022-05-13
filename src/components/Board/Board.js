8acaab7ca8a5c7a6536d07d504c4e57227b8f204
Colton <coltonsmith28@gmail.com>
2022-05-13 06:34:40 -0500

    Added folder structure for board + boardimage

---
+++ src/components/Board/Board.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BoardImage from './BoardImage/BoardImage.js'
+
+const Board = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Board;
---
+++ src/components/Board/BoardImage/BoardImage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BoardImage = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default BoardImage;
